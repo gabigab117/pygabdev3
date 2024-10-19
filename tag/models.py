7468f9e07256bf33046a070f3420406351c61056
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-10-19 18:16:02 +0200

    clean migrations files

--- tag/models.py
+++ tag/models.py
@@ -8,13 +8,13 @@ class BlogTaggedPage(TaggedItemBase):
     content_object = ParentalKey("blog.BlogPage", related_name="tagged_items", on_delete=models.CASCADE)
 
 
-class TagIndexPage(Page):
+class BlogTagIndexPage(Page):
 
     def get_context(self, request):
         from blog.models import BlogPage
 
         tag = request.GET.get("tag")
-        blogpages = BlogPage.objects.filter(tags__name=tag)
+        blogpages = BlogPage.objects.filter(tags__name=tag).live()
 
         context = super().get_context(request)
         context["blogpages"] = blogpages
