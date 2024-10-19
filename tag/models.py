ec1577cbe885f10012bdda7529f303ff9bb19d24
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-10-19 14:16:48 +0200

    add page index page

--- tag/models.py
+++ tag/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from taggit.models import TaggedItemBase
+from wagtail.models import Page
 
 
 class TaggedPage(TaggedItemBase):
     content_object = ParentalKey('wagtailcore.Page', related_name="tagged_items", on_delete=models.CASCADE)
+
+
+class TagIndexPage(Page):
+
+    def get_context(self, request):
+        from blog.models import BlogPage
+        from folio.models import FolioPage
+
+        tag = request.GET.get("tag")
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+        projects = FolioPage.objects.filter(tags__name=tag)
+
+        context = super().get_context(request)
+        context["blogpages"] = blogpages
+        context["projects"] = projects
+        return context
