d4fa872bf0398d7bc01b6d73eb527fb3d4247c93
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-10-31 13:58:29 +0100

    add projects page

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, ProjectList
 
 
 app_name = "customers"
 urlpatterns = [
-    path("dashboard/", dashboard_view, name="dashboard")
+    path("dashboard/", dashboard_view, name="dashboard"),
+    path("projects/", ProjectList.as_view(), name="projects")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
-from .models import Customer
+from .models import Customer, Project
 
 
 @staff_member_required
 def dashboard_view(request):
     customers = Customer.objects.all()
     return render(request, "customers/dashboard.html", context={"customers": customers})
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class ProjectList(ListView):
+    model = Project
+    context_object_name = "projects"
+    template_name = "customers/projects.html"
