2b854dba879aa53fbea4af02657e31db4d3e66c4
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-11-04 20:45:38 +0100

    add secret key in prod

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -3,8 +3,5 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
