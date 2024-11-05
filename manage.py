2c26f4b9821560068c94bd973e6b7901b3b15929
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-11-05 01:27:07 +0100

    update manage to prod

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+from project.settings.base import env
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
+    
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE", default="project.settings.dev"))
 
     from django.core.management import execute_from_command_line
 
