e1573e158fb5589f7724c8abe8d015480a986f6a
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-12-05 18:51:26 +0100

    add tests for services

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,7 +4,7 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
-from customers.models import Invoice
+from customers.models import Invoice, Service
 
 
 @pytest.mark.django_db
@@ -58,3 +58,29 @@ def test_update_invoice_view(client: Client, admin, today, pdf1, invoice_1_with_
     assert response.status_code == 302
     assert Invoice.objects.filter(number=1).exists()
     assert response.url == reverse("customers:invoice", kwargs={"pk": invoice_1_with_services.pk})
+
+
+@pytest.mark.django_db
+def test_create_service_view(client: Client, project_1, admin, today):
+    client.force_login(admin)
+    data = {
+        "date": today,
+        "time_spent": 0.25,
+    }
+    response = client.post(reverse("customers:add-service", kwargs={"pk": project_1.pk}), data=data)
+
+    assert Service.objects.all().count() == 1
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_update_service_view(client: Client, service_1: Service, admin, today):
+    client.force_login(admin)
+    data = {
+        "date": today - datetime.timedelta(days=-1),
+        "time_spent": 0.21,
+    }
+    response = client.post(reverse("customers:update-service", kwargs={"pk": service_1.pk}), data=data)
+    service_1.refresh_from_db()
+    assert service_1.time_spent == 0.21
+    assert response.status_code == 302
