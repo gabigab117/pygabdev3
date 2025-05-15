adcdfbbe3501473cff834bf6b2b32e3369cb850c
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-05-15 22:16:23 +0200

    Mettre à jour la configuration WSGI pour utiliser les variables d'environnement pour le module de paramètres Django.

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import environ
+
+env = environ.Env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
