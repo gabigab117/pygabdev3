721e590b92e6b971b9efa900a0dc24a5575135d3
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-05-16 08:36:11 +0200

    Modifier la configuration de la base de données pour utiliser un nom de fichier par défaut pour SQLite.

--- project/settings/production.py
+++ project/settings/production.py
@@ -21,7 +21,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / env("DB_NAME", default="db.sqlite3"),
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
