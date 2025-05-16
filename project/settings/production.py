b6122829607ef540caaa2de58e391941192fb111
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-05-16 13:59:58 +0200

    Ajouter l'initialisation de Sentry pour la gestion des erreurs et le suivi des performances.

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import sentry_sdk
 
 DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
@@ -47,3 +48,12 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
 }
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn=env("DSN"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
